0fe61846674644c532146bc23470f26b74e59198
Wataru823 <voyage0823@gmail.com>
2023-10-10 23:54:19 +0900

    station15

---
+++ src/scripts/station15.js
@@ -0,0 +1,5 @@
+// ボタンを押すと、div#fuits内がulとliを使ったリスト表示に変化
+function displayList(){
+  var fruits = document.getElementById('fruits');
+  fruits.innerHTML = '<ul><li>リンゴ</li><li>イチゴ</li><li>ブドウ</li></ul>';
+}
